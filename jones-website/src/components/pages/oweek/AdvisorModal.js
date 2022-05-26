a92a9a7fe5f2beda8edd12e30298ea3ae45ac83c
theoschweizer <schweizer.theodore@gmail.com>
2022-05-26 12:47:06 -0700

    changes to advisor modal

--- jones-website/src/components/pages/oweek/AdvisorModal.js
+++ jones-website/src/components/pages/oweek/AdvisorModal.js
@@ -8,12 +8,38 @@ function AdvisorModal(props) {
 }
   return (
     <div className='advisor-modal'>
-      <div className='advisor-img' style={bgImg}/>
-      <div>
-        <h4 className='a-name'>NAME HERE</h4>
-        <p className='a-email'>email@email.com</p>
-        <p className='a-majors'>These are my many majors</p>
+      <div className='a-grid'>
+        <div className='a-container'>
+          <div className='advisor-img' style={bgImg}/>
+          <div>
+            <h4 className='a-name'>NAME HERE</h4>
+            <p className='a-email'>email@email.com</p>
+            <p className='a-majors'>These are my many majors</p>
+            <img src='./college-tags/baker_tag-removebg-preview.png' height={"24px"}/>
+          </div>
+        </div>
+        
+        <div className='a-container'>
+          <div className='advisor-img' style={bgImg}/>
+          <div>
+            <h4 className='a-name'>NAME HERE</h4>
+            <p className='a-email'>email@email.com</p>
+            <p className='a-majors'>These are my many majors</p>
+            <img src='./college-tags/baker_tag-removebg-preview.png' height={"24px"}/>
+          </div>
+        </div>
+
+        <div className='a-container'>
+          <div className='advisor-img' style={bgImg}/>
+          <div>
+            <h4 className='a-name'>NAME HERE</h4>
+            <p className='a-email'>email@email.com</p>
+            <p className='a-majors'>These are my many majors</p>
+            <img src='./college-tags/baker_tag-removebg-preview.png' height={"24px"}/>
+          </div>
+        </div>
       </div>
+      
       
     </div>
   )
