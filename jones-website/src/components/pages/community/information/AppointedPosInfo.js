60255323940c947a0e64cb9e32d1026b72c0577b
hhuang5163 <hhuang5163@gmail.com>
2022-06-22 13:58:00 -0500

    More work on student gov page

---
+++ jones-website/src/components/pages/community/information/AppointedPosInfo.js
@@ -0,0 +1,82 @@
+export const AppointedPos = [
+    {
+        "position": "President",
+        "info": [
+            {
+                "title": "Beer Bike Coordinators",
+                "email": "beerbikejones@gmail.com",
+                "people": ["Mary Brady", "Sofia Roa", "Steven Dillman", "Olivia Goganian", "Lucia Romero-Alston", "Luisa Tolda"]
+            },
+            {
+                "title": "Parliamentarian",
+                "email": "jonesparliamentarian@gmail.com",
+                "people": ["Harry Golen"]
+            },
+            {
+                "title": "Food Rep",
+                "email": "jonescollegefoodrep@gmail.com",
+                "people": []
+            },
+            {
+                "title": "Historians",
+                "email": "thejoneshistorian@gmail.com",
+                "people": []
+            }
+        ]
+    },
+    {
+        "position": "IVP",
+        "info": [
+            {
+                "title": "Ambiance Coordinators",
+                "email": "jones.ambiance@gmail.com",
+                "people": ["Lauren Yu", "Yuv Sachdeva"],
+            },
+            {
+                "title": "Elections Coordinators",
+                "email": "joneselectionsgmail.com",
+                "people": ["Amy Cao", "Dawson Franklin"],
+            },
+            {
+                "title": "Kitchen Coordinators",
+                "email": "joneskitchencommittee@gmail.com",
+                "people": ["Joseph Gioia", "Ryan Dullea"],
+            },
+            {
+                "title": "Merchandise Coordinators",
+                "email": "jonesmerchcoordinators@gmail.com",
+                "people": ["Annie Li", "Celine Choi", "Daniel Joshua", "Lauren Yu", "Maya Hoffman", "Sarah Luan"],
+            },
+            {
+                "title": "PR Director",
+                "email": "jonesprdirector@gmail.com",
+                "people": [],
+            },
+            {
+                "title": "Sports Coordinator",
+                "email": "jonessportscoords@gmail.com",
+                "people": [],
+            }
+        ]
+    },
+    {
+        "position": "EVP",
+        "info": [
+            {
+                "title": "Community Involvement Coordinators",
+                "email": "jones.cic5@gmail.com",
+                "people": ["Darren Wang", "Rija Naqvi"]
+            },
+            {
+                "title": "Arts and Cultural Coordinators",
+                "email": "jonesculturals@gmail.com",
+                "people": ["Andrea Gomez", "Hafsa Ahsan", "Edelawit Negash", "Eduardo Ruiz"]
+            },
+            {
+                "title": "Webmagisters",
+                "email": "jibawebmaster@gmail.com",
+                "people": ["Theo Schweizer", "Katherine Chui", "Harrison Huang", "Ron Underwood"]
+            }
+        ]
+    }
+]
