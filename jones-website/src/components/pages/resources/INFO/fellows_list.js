1027284483e0051988d6dc6d2071b1d35799d2fd
Ananya Rao <ananya.rao@gmail.com>
2025-09-16 11:47:04 -0500

    updating fellows and paas

--- jones-website/src/components/pages/resources/INFO/fellows_list.js
+++ jones-website/src/components/pages/resources/INFO/fellows_list.js
@@ -1,107 +1,117 @@
 export const FellowsList = [
     {
-        "name":"NAME",
-        "subjects":"FELLOWING",
-        "email":"EMAIL",
-    }, 
-    {
-        "name": "Anna Huang",
-        "subjects": "BUSI 305, ECON 200/203",
-        "email": "ah118@rice.edu"
-    },
-    {
-        "name": "Ana Letca",
-        "subjects": "BIOS 201/202",
-        "email": "aal10@rice.edu"
-    },
-    {
-        "name": "Nyla Hartigan",
-        "subjects": "MATH 211",
-        "email": "nrh1@rice.edu"
-    },
-    {
-        "name": "Matthew Taylor-Chang",
-        "subjects": "CHEM 211/313",
-        "email": "mjt9@rice.edu"
-    },
-    {
         "name": "Kathy Lachman",
-        "subjects": "MATH 101",
+        "subjects": "BIOS 201/301/302/353, CHEM 121/122/211/320",
         "email": "kl116@rice.edu"
     },
     {
-        "name": "Gretchen Schulke",
-        "subjects": "PHYS 101/102",
-        "email": "gns4@rice.edu"
-    },
-    {
-        "name": "Shrutika Gupta",
-        "subjects": "Upper Level BIOS",
-        "email": "sg121@rice.edu"
-    },
-    {
-        "name": "Varsha Chandramouli",
-        "subjects": "MATH 212",
-        "email": "vkc1@rice.edu"
-    },
-    {
-        "name": "Devika Shankar",
-        "subjects": "BOIS 301",
-        "email": "ds82@rice.edu"
-    },
-    {
-        "name": "Eduardo Ruiz",
-        "subjects": "BIOS 301",
-        "email": "er36@rice.edu"
-    },
-    {
-        "name": "Rija Naqvi",
-        "subjects": "CHEM 211",
-        "email": "rhn1@rice.edu"
-    },
-    {
-        "name": "Ben Lindenbaum",
-        "subjects": "COMP 182",
-        "email": "bl77@rice.edu"
-    },
-    {
-        "name": "Sean McGarry",
-        "subjects": "STAT",
-        "email": "spm10@rice.edu"
-    },
-    {
-        "name": "Mira Srinivasa",
-        "subjects": "BIOE",
-        "email": "mas50@rice.edu"
-    },
-    {
-        "name": "Arcadia Hinton-Cooley",
-        "subjects": "Writing",
-        "email": "arh8@rice.edu"
-    },
-    {
-        "name": "James Cheng",
-        "subjects": "PHYS 125/126",
-        "email": "jdc15@rice.edu"
-    },
-    {
-        "name": "Anjali Mathew",
-        "subjects": "COMP 140",
-        "email": "am307@rice.edu"
+        "name": "Nyla Hartigan",
+        "subjects": "PHYS 111/112, MATH 211/212, any EEPS",
+        "email": "nrh1@rice.edu"
     },
     {
         "name": "Samvita Gautham",
-        "subjects": "CHEM 121/122",
+        "subjects": "BIOS 300/301, CHEM 121/122, SPAN 330/368, PHYS 125, MDHM 201/268, ENGL 200/330",
         "email": "sg185@rice.edu"
     },
     {
-        "name": "Melissa Lee",
-        "subjects": "KINE",
-        "email": "mcl15@rice.edu"
+        "name": "Varsha Chandramouli",
+        "subjects": "CHEM 211/313, BIOS 372/301/334, MATH 212, KINE 301/300/351/302",
+        "email": "vkc1@rice.edu"
+    },
+    {
+        "name": "Mira Srinivasa",
+        "subjects": "CHEM 211/313, BIOS 341, BIOE 252/322/391/332/372/370",
+        "email": "mas50@rice.edu"
+    },
+    {
+        "name": "Sean McGarry",
+        "subjects": "Any STAT, low level SMGT & BUSINESS",
+        "email": "spm10@rice.edu"
+    },
+    {
+        "name": "Arcadia Hinton-Cooley",
+        "subjects": "Writing, SOSC 302, PSYC 101/202/203/340",
+        "email": "arh8@rice.edu"
+    },
+    {
+        "name": "Zack Donovan",
+        "subjects": "CMOR 220/302/304/360, MATH 101/102/212/221/212/222/270/302/321/354/355",
+        "email": "zd25@rice.edu"
+    },
+    {
+        "name": "Ana Letca",
+        "subjects": "BIOS 201/202/301/341, CHEM 211, PHYS 125/126",
+        "email": "aal10@rice.edu"
+    },
+    {
+        "name": "Anjali Mathew",
+        "subjects": "COMP 140, ECON 100, COMP 182, ECON 200/203, COMP 215/312",
+        "email": "am307@rice.edu"
+    },
+    {
+        "name": "Ben Lindenbaum",
+        "subjects": "COMP 140/182/215/312",
+        "email": "bl77@rice.edu"
     },
     {
         "name": "Biraj Nainabasti",
-        "subjects": "CHEM 121/122",
+        "subjects": "BIOS 201/202/301, CHEM 211, PHYS 126",
         "email": "bn25@rice.edu"
     },
+    {
+        "name": "Melissa Lee",
+        "subjects": "BIOS 202, KINE 300/301/310/311/319/321/440, HEAL 103",
+        "email": "mcl15@rice.edu"
+    },
+    {
+        "name": "Jackson O'Brien",
+        "subjects": "MECH 202/310, CHEM 121/211, CMOR 220, MATH 211",
+        "email": "jo60@rice.edu"
+    },
+    {
+        "name": "Coleman O'Brien",
+        "subjects": "MECH 202/200, CMOR 220, MATH 212/211",
+        "email": "co63@rice.edu"
+    },
+    {
+        "name": "Leslie Rojo Flores",
+        "subjects": "PSYC 101/203/202, SOCI 101, FWIS, Spanish all levels, CMOR 220",
+        "email": "lr80@rice.edu"
+    },
+    {
+        "name": "Rebecca Tang",
+        "subjects": "CHEM 211, PSYC 202, FWIS/writing, STAT 310, ASIA 216",
+        "email": "rt66@rice.edu"
+    },
+    {
+        "name": "Simran Ghosh",
+        "subjects": "STAT 410, ECON 307/455, Creative/fiction writing",
+        "email": "sg256@rice.edu"
+    },
+    {
+        "name": "Ankita Rajesh",
+        "subjects": "ECON 100, DSCI 101, CHEM 211",
+        "email": "ar228@rice.edu"
+    },
+    {
+        "name": "Gwendolyn Crain",
+        "subjects": "English, Poli Sci, Writing",
+        "email": "gc63@rice.edu"
+    },
+    {
+        "name": "Tori Cole",
+        "subjects": "POLI 211, POLI 210, Writing/FWIS",
+        "email": "tmc16@rice.edu"
+    },
+    {
+        "name": "Ryan Hartgerink",
+        "subjects": "Pre-law related classes, Cog Sci",
+        "email": "reh6@rice.edu"
+    },
+    {
+        "name": "Eshika Tandon",
+        "subjects": "BIOS 201, BIOS 341, BIOS 385",
+        "email": "et58@rice.edu"
+    }
 ]
--- jones-website/src/components/pages/resources/INFO/paas_list.js
+++ jones-website/src/components/pages/resources/INFO/paas_list.js
@@ -1,135 +1,116 @@
 export const PAAs = [
     {
-        name: "Maya Hoffman", 
-        email: "mh106@rice.edu", 
-        areaOfStudy: "Health Sciences; Biochemistry & Cell Biology minor, Medical Humanities minor; Pre-Med", 
-        year: "Senior"
-    }, 
-    {
-        name: "Hannah Lloyd", 
-        email: "hal12@rice.edu", 
-        areaOfStudy: "Sports Medicine and Exercise Physiology; Biochemistry & Cell Biology minor; Spanish Certificate; Pre-Dental", 
-        year: "Senior"
-    }, 
-    {
-        name: "Eduardo Ruiz", 
-        email: "er36@rice.edu", 
-        areaOfStudy: "BioChemistry; Medical Humanities minor; Pre-Med", 
-        year: "Senior"
-    }, 
-    {
-        name: "Seryna Ayala", 
-        email: "ssa10@rice.edu", 
-        areaOfStudy: "BioSciences; Art History minor; Pre-Med", 
-        year: "Senior"
-    }, 
-    {
-        name: "Tolda Bustamante", 
-        email: "lb50@rice.edu", 
-        areaOfStudy: "Political Science and Visual and Dramatic Arts", 
-        year: "Senior"
-    }, 
-    {
-        name: "Ric Chang", 
-        email: "ec63@rice.edu", 
-        areaOfStudy: "Chemical & Biomolecular Engineering; Engineering Design minor; Pre-med", 
-        year: "Junior"
-    }, 
-    {
-        name: "Varsha Chandramouli", 
-        email: "vkc1@rice.edu", 
-        areaOfStudy: "Biosciences and Spanish; Medical Humanities Minor; Pre-Med", 
-        year: "Junior"
-    }, 
-    {
-        name: "Ric Chang", 
-        email: "ec63@rice.edu", 
-        areaOfStudy: "Chemical and Biomolecular Engineering; Engineering Design Minor; Philosophy Minor", 
-        year: "Senior"
-    }, 
-    {
-        name: "Prahalad Chari", 
-        email: "pac9@rice.edu", 
-        areaOfStudy: "Electrical Engineering, Pre-Graduate", 
-        year: "Senior"
-    }, 
-    {
-        name: "Margo Gee", 
-        email: "mjg11@rice.edu", 
-        areaOfStudy: "Linguistics, Anthropology; Engineering Design minor", 
-        year: "Senior"
-    }, 
-    {
-        name: "James Cheng", 
-        email: "jdc15@rice.edu", 
-        areaOfStudy: "Biosciences; Neuroscience Minor; Pre-Med", 
-        year: "Senior"
-    }, 
-    {
-        name: "Amy Coa", 
-        email: "ayc8@rice.edu", 
-        areaOfStudy: "Economics and Statistics; Financial Computation and Modeling Minor; Creative Writing Minor", 
-        year: "Senior"
-    }, 
-    {
-        name: "Nyla Hartigan", 
-        email: "nrh1@rice.edu", 
-        areaOfStudy: "Physics and/or EEPS; Data Science Minor", 
-        year: "Junior"
-    }, 
-    {
-        name: "Abigail Li", 
-        email: "al181@rice.edu", 
-        areaOfStudy: "Sports Medicine and Exercise Physiology; Spanish; Pre-Med", 
+        name: "Abigail Li",
+        email: "al181@rice.edu",
+        areaOfStudy: "Sports Medicine & Exercise Physiology; Spanish minor; Biochemistry & Cell Biology minor; Pre-Med",
         year: "Sophomore"
-    }, 
+    },
     {
-        name: "Arryana Lyon", 
-        email: "anl10@rice.edu", 
-        areaOfStudy: "Cognitive Sciences; Environmental Studies Minor; Civic Leadership Certificate; French Language Certificate; Pre-Law ", 
+        name: "Arryana Lyon",
+        email: "anl10@rice.edu",
+        areaOfStudy: "Cognitive Sciences; Environmental Studies/French Language/Civic Leadership minors; Pre-Law, Pre-Graduate School",
         year: "Junior"
-    }, 
+    },
     {
-        name: "Rija Naqvi", 
-        email: "rhn1@rice.edu", 
-        areaOfStudy: "Biosciences; Politics, Law, and Social Thought Minor; Medical Humanities Minor; Pre-Med", 
-        year: "Senior"
-    }, 
+        name: "Mira Srinivasa",
+        email: "mas50@rice.edu",
+        areaOfStudy: "Bioengineering; Pre-Med",
+        year: "Junior"
+    },
     {
-        name: "Kareena Patel", 
-        email: "khp4@rice.edu", 
-        areaOfStudy: "Sports Medicine and Excercise Physiology; Pre-Med", 
-        year: "Senior"
-    }, 
+        name: "Jackson O'Brian",
+        email: "jo60@rice.edu",
+        areaOfStudy: "Mechanical Engineering; Pre-Med",
+        year: "Freshman"
+    },
     {
-        name: "Andrea Rodriguez", 
-        email: "ar221@rice.edu", 
-        areaOfStudy: "Political Science; Pre-Law", 
+        name: "Coleman O'Brian",
+        email: "co63@rice.edu",
+        areaOfStudy: "Mechanical Engineering; Pre-Law",
+        year: "Freshman"
+    },
+    {
+        name: "Ben Schussler",
+        email: "bns5@rice.edu",
+        areaOfStudy: "Mathematics; Physics; Linguistics; Pre-Graduate School",
+        year: "Junior"
+    },
+    {
+        name: "Nyla Hartigan",
+        email: "nrh1@rice.edu",
+        areaOfStudy: "EEPS; Physics; Data Science minor; Pre-Graduate School",
+        year: "Junior"
+    },
+    {
+        name: "Hannah Watkins",
+        email: "hw76@rice.edu",
+        areaOfStudy: "Computer Science",
+        year: "Junior"
+    },
+    {
+        name: "Varsha Chandramoulli",
+        email: "vkc1@rice.edu",
+        areaOfStudy: "Sports Medicine & Exercise Physiology; Spanish minor; Biochemistry & Cell Biology minor; Medical Humanities minor; Pre-Med",
+        year: "Junior"
+    },
+    {
+        name: "Lily Hestjean",
+        email: "lh70@rice.edu",
+        areaOfStudy: "Biosciences (Ecology and Evolutionary Biology); Spanish and Portuguese minor; Environmental Studies minor; Pre-Graduate School",
+        year: "Freshman"
+    },
+    {
+        name: "Leslie Rojo Flores",
+        email: "ls48@rice.edu",
+        areaOfStudy: "Psychology; Sociology; Certificate in Language and Intercultural Communication - Spanish; Poverty, Justice, and Human Capabilities minor",
+        year: "Freshman"
+    },
+    {
+        name: "Elena Wang",
+        email: "ew76@rice.edu",
+        areaOfStudy: "Physics; Data Science minor; Pre-Graduate School",
+        year: "Freshman"
+    },
+    {
+        name: "Ava Brazell",
+        email: "amb36@rice.edu",
+        areaOfStudy: "Political Science major; Business minor; Pre-Law",
         year: "Sophomore"
-    }, 
-    {
-        name: "Ithzel Rubio", 
-        email: "ir22@rice.edu", 
-        areaOfStudy: "Sociology; Politics, Law, and Social Thought Minor; Study of Women, Gender, and Sexuality Minor; Pre-Law", 
-        year: "Junior"
-    }, 
-    {
-        name: "Mira Srinivasa", 
-        email: "mas50@rice.edu", 
-        areaOfStudy: "Bioengineering; Pre-MD/PhD", 
-        year: "Junior"
     },
     {
-        name: "Hannah Watkins", 
-        email: "hw76@rice.edu", 
-        areaOfStudy: "Computer Science", 
-        year: "Junior"
+        name: "Tori Cole",
+        email: "tmc16@rice.edu",
+        areaOfStudy: "Political Science; Sociology; African American Studies minor",
+        year: "Sophomore"
     },
     {
-        name: "Lauren Yu", 
-        email: "ly39@rice.edu", 
-        areaOfStudy: "Computer Science; Data Science Minor; Biochemistry and Cell Biology Minor", 
+        name: "Annie Yan",
+        email: "ay63@rice.edu",
+        areaOfStudy: "Managerial Economics; Political Science/Social Policy Analysis; Politics, Law and Social Thought minor; Pre-Law",
         year: "Senior"
     },
-     
+    {
+        name: "Rachel Ayalon",
+        email: "ra106@rice.edu",
+        areaOfStudy: "Political Science; Social Policy Analysis; Spanish and Portuguese minor",
+        year: "Freshman"
+    },
+    {
+        name: "Yilan Ruan",
+        email: "yr23@rice.edu",
+        areaOfStudy: "Mathematics; Computer Science; Pre-Graduate School",
+        year: "Junior"
+    },
+    {
+        name: "Biraj Nainabasti",
+        email: "bn25@rice.edu",
+        areaOfStudy: "Sports Medicine; Biochemistry minor; Pre-Med",
+        year: "Sophomore"
+    },
+    {
+        name: "Naayaa Mehta",
+        email: "nm80@rice.edu",
+        areaOfStudy: "Bioengineering; Global Health Technologies minor; Pre-Graduate School",
+        year: "Sophomore"
+    }  
 ]
