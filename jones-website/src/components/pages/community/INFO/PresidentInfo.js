b9f93ae0cf86d45c22a27ab4d334678f364f2d2b
Ananya Rao <ananya.rao@gmail.com>
2026-03-13 13:25:19 -0500

    updated pres info w/ ava

--- jones-website/src/components/pages/community/INFO/PresidentInfo.js
+++ jones-website/src/components/pages/community/INFO/PresidentInfo.js
@@ -1,6 +1,6 @@
 export const PresInfo = {
-    "img": "./images/cabinet/2025-2026/Zack.JPG",
-    "name": "Zack Donovan",
-    "year": "2025-2026",
+    "img": "./images/cabinet/2026-2027/ava.jpeg",
+    "name": "Ava Brazell",
+    "year": "2026-2027",
     "desc": ""
 }
